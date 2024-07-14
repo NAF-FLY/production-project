bfb7feba9648a83cb1ed0c866ace443c48f78da9
vpatsyuk <vpacyuk@team.amocrm.com>
2024-07-14 23:39:31 +0300

    add config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,19 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildPlugins({
+  paths,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+  ];
+}
