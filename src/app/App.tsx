2aa1d8745186e6a418219b53d5a24f5fe32e07e5
vpatsyuk <vpacyuk@team.amocrm.com>
2024-07-16 17:15:25 +0300

    fsd structure

---
+++ src/app/App.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+
+import "./styles/index.scss";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div
+      className={classNames("app", { hovered: true, selected: false }, [theme])}
+    >
+      <Navbar />
+      <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
+    </div>
+  );
+};
+export default App;
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,27 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+
+import cls from "./Navbar.module.scss";
+
+interface INavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: INavbarProps) => {
+  return (
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={"/"}
+          className={cls.mainLink}
+        >
+          Главная
+        </AppLink>
+        <AppLink theme={AppLinkTheme.RED} to={"/about"}>
+          О сайте
+        </AppLink>
+      </div>
+    </div>
+  );
+};
